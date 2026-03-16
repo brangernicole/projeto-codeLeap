d69352c1d2cddecd51b2891e412e2a1babba31ec
Wellington Oliveira <brangernicole@gmail.com>
2026-03-16 13:31:51 -0300

    fixing enter button

--- projeto-codeleap/src/pages/SignUp.tsx
+++ projeto-codeleap/src/pages/SignUp.tsx
@@ -21,27 +21,28 @@ export default function SignUp() {
         </h1>
 
         <p className="text-sm mb-1">Please enter your username</p>
-      <input
-      type="text"
-      placeholder="John Doe"
-      value={username}
-      onChange={(e) => setUsername(e.target.value)}
-      className="w-full h-8 border border-gray-400 rounded-lg mb-4 px-2 text-sm focus:border-gray-500 outline-none"
-    />
-
-    <form onSubmit={handleSubmit} className="flex justify-end">
-      <button
-        type="submit"
-        disabled={!username.trim()}
-        className={`w-full max-w-[111px] h-8 rounded-lg flex items-center justify-center font-bold text-white ${
-          username.trim()
-            ? "bg-blue-400 hover:bg-blue-500 cursor-pointer"
-            : "bg-gray-400 hover:bg-gray-500 cursor-not-allowed"
-        }`}
-      >
-        ENTER
-      </button>
-    </form>
+       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+  <input
+    type="text"
+    placeholder="John Doe"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)}
+    className="w-full h-8 border border-gray-400 rounded-lg mb-4 px-2 text-sm focus:border-gray-500 outline-none"
+  />
+  <div className="flex justify-end">
+    <button
+      type="submit"
+      disabled={!username.trim()}
+      className={`w-full max-w-[111px] h-8 rounded-lg flex items-center justify-center font-bold text-white ${
+        username.trim()
+          ? "bg-blue-400 hover:bg-blue-500 cursor-pointer"
+          : "bg-gray-400 hover:bg-gray-500 cursor-not-allowed"
+      }`}
+    >
+      ENTER
+    </button>
+  </div>
+</form>
 
       </div>
     </div>
